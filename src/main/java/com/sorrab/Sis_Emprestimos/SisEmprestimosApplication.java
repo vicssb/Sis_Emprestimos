b2c6b5e5a09ef71e18845134907d99a44a4d8541
Victor <vicssb@gmail.com>
2024-04-19 14:53:58 -0300

    Initial commit

---
+++ src/main/java/com/sorrab/Sis_Emprestimos/SisEmprestimosApplication.java
@@ -0,0 +1,13 @@
+package com.sorrab.Sis_Emprestimos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisEmprestimosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisEmprestimosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sorrab/Sis_Emprestimos/SisEmprestimosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sorrab.Sis_Emprestimos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisEmprestimosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
